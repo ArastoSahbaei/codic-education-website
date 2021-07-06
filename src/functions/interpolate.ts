e65a253c81a9a174be344e0bef371f594166e9da
Sebastian Karlsson <ianertson@gmail.com>
2021-07-06 15:22:47 +0200

    smaller interpolate function

--- src/functions/interpolate.ts
+++ src/functions/interpolate.ts
@@ -1,6 +1,6 @@
 /**
  * method of constructing (finding) new data points based on
- * the range of a discrete set of known data points
+ *  the range of a discrete set of known data points
  */
 export const interpolate = (start: number, end: number, precision: number) => {
 	const n =
