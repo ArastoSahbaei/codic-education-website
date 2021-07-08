c6d8788596715fbb3ff418f8bc1161f7d6e3a797
arasto <Arasto.Sahbaei@gmail.com>
2021-07-08 11:36:16 +0200

    format fix

--- src/components/space/index.tsx
+++ src/components/space/index.tsx
@@ -1,10 +1,8 @@
 import { FC } from 'react'
-import styled from 'styled-components'
 import { ISpace } from './types'
+import styled from 'styled-components'
 
-
-export const Space:FC<ISpace> = (props: ISpace) => <Wrapper space={props.space} />
-
+export const Space: FC<ISpace> = (props: ISpace) => <Wrapper space={props.space} />
 
 const Wrapper = styled.div`
     margin-top: ${(props: Partial<ISpace>) => props.space}rem
--- src/components/space/types.ts
+++ src/components/space/types.ts
@@ -1,3 +1,3 @@
 export interface ISpace {
-    space:number
+	space: number
 }
