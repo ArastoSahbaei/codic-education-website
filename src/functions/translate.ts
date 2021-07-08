737869c59d34987fcd354803e6e4a0bf0a094b64
Sebastian Karlsson <ianertson@gmail.com>
2021-07-08 17:15:19 +0200

    remove unused file

--- src/functions/translate.ts
+++ src/functions/translate.ts
@@ -2,4 +2,4 @@ import translations from '../shared/data/translations.json'
 
 const translationsObj = Object(translations)
 
-export const translate = (text: string) => text in translationsObj ? translationsObj[text] : text
+export const translate = (text?: string) => text && text in translationsObj ? translationsObj[text] : text
