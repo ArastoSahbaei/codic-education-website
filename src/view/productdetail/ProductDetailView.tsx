80b6c5e019f29adab217fe082f680a8305c3418e
arasto <Arasto.Sahbaei@gmail.com>
2021-07-09 11:55:24 +0200

    will now properly fetch data

--- src/view/productdetail/ProductDetailView.tsx
+++ src/view/productdetail/ProductDetailView.tsx
@@ -1,12 +1,16 @@
 import { useLocation } from 'react-router-dom'
 import { useFetch } from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
 import { Spinner } from '../../components/Spinner'
 import { Product } from '../../shared/interfaces/ProductsInterface'
 import CodicAPIService from '../../shared/api/services/CodicAPIService'
 
+interface value { id: string }
+
 export const ProductDetailView: React.FC = () => {
 	const location = useLocation<Product>()
-	const { data, loading, error } = useFetch(CodicAPIService.getProductByID('60e406cab4843a4d10746354'))
+	const { id } = useParams<value>()
+	const { data, loading, error } = useFetch(CodicAPIService.getProductByID(id))
 
 	return (
 		loading
@@ -18,7 +22,7 @@ export const ProductDetailView: React.FC = () => {
 					<span>{location.state ? location.state.productCategoryName : data?.productCategoryName}</span> <br />
 					<span>{location.state ? location.state.quantity : data?.quantity}</span> <br />
 					<span>{location.state ? location.state.title : data?.title}</span> <br />
-					{/* 	<button onClick={() => addToCart(location.state._id)}>Add to cart</button> */}
+					<button /* onClick={() => addToCart(location.state._id)} */>Add to cart</button>
 				</>
 			)
 	)
