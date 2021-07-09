0b717d261a81e946348f16c3f551aa63b4532b22
arasto <Arasto.Sahbaei@gmail.com>
2021-07-09 13:52:28 +0200

    removed junk

--- src/view/signin/SignInView.tsx
+++ src/view/signin/SignInView.tsx
@@ -1,42 +1,31 @@
-import React, { useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { useHistory } from 'react-router'
-import styled from 'styled-components'
-import RoutingPath from '../../routes/RoutingPath'
-import CodicAPIService from '../../shared/api/services/CodicAPIService'
-import LocalStorage from '../../shared/cache/LocalStorage'
-import { windowsMaxWidth } from '../../shared/data/WindowsSizes'
-import initialImage from '../../shared/images/teacher2.jpg'
+import { UserContext } from '../../shared/providers/UserProvider'
 import { DimensionsInterface } from '../../shared/interfaces/DimensionsInterface'
 import { RegisterNewUserData, RegisterNewUserFunction, SignInProps } from '../../shared/interfaces/SingInInterface'
-import { LoginCredentials, RegisterNewUser } from '../../shared/interfaces/UserInterface'
-import { UserContext } from '../../shared/providers/UserProvider'
 import { primaryBackgroundColor, primaryColor, secondaryColor } from '../../shared/styles/GlobalStyle'
+import { LoginCredentials, RegisterNewUser } from '../../shared/interfaces/UserInterface'
 import { RegisterUser } from './components/RegisterUser'
 import { SignInUser } from './components/SignInUser'
+import { windowsMaxWidth } from '../../shared/data/WindowsSizes'
+import CodicAPIService from '../../shared/api/services/CodicAPIService'
+import LocalStorage from '../../shared/cache/LocalStorage'
+import initialImage from '../../shared/images/teacher2.jpg'
+import RoutingPath from '../../routes/RoutingPath'
+import styled from 'styled-components'
 
 export const SignInView = () => {
 	const history = useHistory()
-	const [, setAuthenticatedUser] = useContext(UserContext)
-	const [registerUser, setRegisterUser] = useState<RegisterNewUser>({
-		username: '',
-		password: '',
-		email: '',
-		receiveNewsLetters: true
-	})
-	const [loginCredentials, setLoginCredentials] = useState<LoginCredentials>({username: '', password: ''})
+	const [setAuthenticatedUser] = useContext(UserContext)
+	const [registerUser, setRegisterUser] = useState<RegisterNewUser>({ username: '', password: '', email: '', receiveNewsLetters: true })
+	const [loginCredentials, setLoginCredentials] = useState<LoginCredentials>({ username: '', password: '' })
+	const [showRecoverPasswordView, setShowRecoverPasswordView] = useState<boolean>(false)
 	const [loginView, setLoginView] = useState<boolean>(true)
-	const [loginHeaderText] = useState<string>('Logga in')
-	const [registerHeader] = useState<string>('Registrera dig')
-	const [usernameText] = useState<string>('Användarnamn')
-	const [emailText] = useState<string>('Email')
-	const [passwordText] = useState<string>('Lösenord')
-	const [passwordConfirmText] = useState<string>('Bekräfta lösenord')
-	const [passwordMismatched] = useState<string>('Lösenorden matchar ej!')
-	
+
 	const signIn = async (event: React.MouseEvent<HTMLElement>) => {
 		event.preventDefault()
 		try {
-			const {data} = await CodicAPIService.login(loginCredentials)
+			const { data } = await CodicAPIService.login(loginCredentials)
 			localStorage.setItem(LocalStorage.authenticationToken, data.token)
 			console.log(data)
 			setAuthenticatedUser(data)
@@ -45,7 +34,7 @@ export const SignInView = () => {
 			console.log(error)
 		}
 	}
-	
+
 	const register = async (event: React.MouseEvent<HTMLElement>) => {
 		event.preventDefault()
 		try {
@@ -57,8 +46,8 @@ export const SignInView = () => {
 			alert(error)
 		}
 	}
-	
-	const registerNewUser:RegisterNewUserFunction = (username, email, password, newsLetter, event) => {
+
+	const registerNewUser: RegisterNewUserFunction = (username, email, password, newsLetter, event) => {
 		console.log(username, email, password, newsLetter)
 		setRegisterUser({
 			username: username,
@@ -68,19 +57,19 @@ export const SignInView = () => {
 		})
 		register(event)
 	}
-	
+
 	const registerData: RegisterNewUserData = {
 		data: {
-			registerHeaderText: registerHeader,
-			usernameText: usernameText,
-			passwordText: passwordText,
-			passwordConfirmText: passwordConfirmText,
-			emailText: emailText,
-			passwordMismatchedText: passwordMismatched,
+			registerHeaderText: 'Registrera dig',
+			usernameText: 'Användarnamn',
+			passwordText: 'Lösenord',
+			passwordConfirmText: 'Bekräfta lösenord',
+			emailText: 'Email',
+			passwordMismatchedText: 'Lösenorden matchar ej!',
 			registerNewUser: registerNewUser,
 		}
 	}
-	
+
 	const logInUser = (username: string, password: string, event: React.MouseEvent<HTMLElement>) => {
 		console.log(username, password)
 		setLoginCredentials({
@@ -90,54 +79,53 @@ export const SignInView = () => {
 		})
 		signIn(event)
 	}
-	
-	const [showRecoverPasswordView, setShowRecoverPasswordView] = useState<boolean>(false)
-	
+
+
 	const changeRecoverPasswordView = () => {
 		setShowRecoverPasswordView(!showRecoverPasswordView)
 	}
-	
+
 	const sendRecoverPasswordEmail = (email: string, event: React.MouseEvent<HTMLElement>) => {
 		event.preventDefault()
 		alert(`Password reset email was sent to "${email}" if account exists`)
 	}
-	
+
 	const signInUserData: SignInProps = {
 		data: {
-			loginHeaderText: loginHeaderText,
-			usernameText: usernameText,
-			passwordText: passwordText,
-			emailText: emailText,
+			loginHeaderText: 'Logga in',
+			usernameText: 'Användarnamn',
+			passwordText: 'Lösenord',
+			emailText: 'Email',
 			logInUser: logInUser,
 			showRecoverPasswordView: showRecoverPasswordView,
 			changeRecoverPasswordView: changeRecoverPasswordView,
 			sendRecoverPasswordEmail: sendRecoverPasswordEmail
 		}
 	}
-	
+
 	return (
 		<Wrapper>
-			<Image src={ initialImage } windowsMaxWidth={ windowsMaxWidth } loginView={ loginView }/>
-			<MainWrapper dimensions={ windowsMaxWidth }>
+			<Image src={initialImage} windowsMaxWidth={windowsMaxWidth} loginView={loginView} />
+			<MainWrapper dimensions={windowsMaxWidth}>
 				<TitleWrapper>
 					<TitleWrapperItem
-						loginView={ loginView }
-						primaryColor={ primaryBackgroundColor }
-						secondaryColor={ secondaryColor }
-						onClick={ () => setLoginView(true) }>
-						<Paragraph>{ loginHeaderText }</Paragraph>
+						loginView={loginView}
+						primaryColor={primaryBackgroundColor}
+						secondaryColor={secondaryColor}
+						onClick={() => setLoginView(true)}>
+						<Paragraph>{'Logga in'}</Paragraph>
 					</TitleWrapperItem>
 					<TitleWrapperItem
-						loginView={ !loginView }
-						primaryColor={ primaryBackgroundColor }
-						secondaryColor={ secondaryColor }
-						onClick={ () => setLoginView(false) }>
-						<Paragraph>{ registerHeader }</Paragraph>
+						loginView={!loginView}
+						primaryColor={primaryBackgroundColor}
+						secondaryColor={secondaryColor}
+						onClick={() => setLoginView(false)}>
+						<Paragraph>{'Registrera dig'}</Paragraph>
 					</TitleWrapperItem>
 				</TitleWrapper>
-				{ loginView
-					? <SignInUser data={ signInUserData.data }/>
-					: <RegisterUser data={ registerData.data }/> }
+				{loginView
+					? <SignInUser data={signInUserData.data} />
+					: <RegisterUser data={registerData.data} />}
 			</MainWrapper>
 		</Wrapper>
 	)
@@ -157,32 +145,32 @@ const Image = styled.img<ImageValues>`
   z-index: -1;
   grid-column: 1/21;
   width: 100%;
-  @media (max-width: ${ props => props.windowsMaxWidth.mobile }) {
+  @media (max-width: ${props => props.windowsMaxWidth.mobile}) {
     margin-top: -10vh;
     margin-bottom: -0vh;
     height: 100vh;
   }
-  @media (min-width: ${ props => props.windowsMaxWidth.tablet }) {
+  @media (min-width: ${props => props.windowsMaxWidth.tablet}) {
     margin-top: -8vh;
-    margin-bottom: ${ props => props.loginView ? '-30vh' : '-5vh' };
+    margin-bottom: ${props => props.loginView ? '-30vh' : '-5vh'};
   }
-  @media (min-width: ${ props => props.windowsMaxWidth.laptop }) {
+  @media (min-width: ${props => props.windowsMaxWidth.laptop}) {
     margin-top: -7vh;
-    margin-bottom: ${ props => props.loginView ? '-15vh' : '-15vh' };
+    margin-bottom: ${props => props.loginView ? '-15vh' : '-15vh'};
   }
-  @media (min-width: ${ props => props.windowsMaxWidth.desktop }) {
+  @media (min-width: ${props => props.windowsMaxWidth.desktop}) {
     margin-top: -10vh;
     margin-bottom: -47vh;
   }
-  @media (min-width: ${ props => props.windowsMaxWidth.qHd }) {
+  @media (min-width: ${props => props.windowsMaxWidth.qHd}) {
     margin-top: -10vh;
     margin-bottom: -38vh;
   }
-  @media (min-width: ${ props => props.windowsMaxWidth.ultraWide }) {
+  @media (min-width: ${props => props.windowsMaxWidth.ultraWide}) {
     margin-top: -15vh;
     margin-bottom: -79vh;
   }
-  @media (min-width: ${ props => props.windowsMaxWidth.ultraHd }) {
+  @media (min-width: ${props => props.windowsMaxWidth.ultraHd}) {
     margin-top: -11vh;
     margin-bottom: -28vh;
   }
@@ -191,37 +179,37 @@ const Image = styled.img<ImageValues>`
 const MainWrapper = styled.div<DimensionsInterface>`
   position: absolute;
   border-radius: 0.5em;
-  background-color: ${ primaryBackgroundColor };
+  background-color: ${primaryBackgroundColor};
   color: white;
   text-align: center;
-  @media (max-width: ${ props => props.dimensions.mobile }) {
+  @media (max-width: ${props => props.dimensions.mobile}) {
     margin-top: 5vh;
     width: 90%;
     margin-left: 5%;
     margin-right: auto;
   }
-  @media (min-width: ${ props => props.dimensions.tablet }) {
+  @media (min-width: ${props => props.dimensions.tablet}) {
     margin-top: 3vh;
     margin-left: 10%;
     width: 400px;
   }
-  @media (min-width: ${ props => props.dimensions.laptop }) {
+  @media (min-width: ${props => props.dimensions.laptop}) {
     margin-top: 8vh;
   }
-  @media (min-width: ${ props => props.dimensions.desktop }) {
+  @media (min-width: ${props => props.dimensions.desktop}) {
     margin-top: 10vh;
     margin-left: 15%;
   }
-  @media (min-width: ${ props => props.dimensions.qHd }) {
+  @media (min-width: ${props => props.dimensions.qHd}) {
     margin-top: 13vh;
     margin-left: 25%;
   }
-  @media (min-width: ${ props => props.dimensions.ultraWide }) {
+  @media (min-width: ${props => props.dimensions.ultraWide}) {
     margin-top: 20vh;
     margin-left: 30%;
     width: 400px;
   }
-  @media (min-width: ${ props => props.dimensions.ultraHd }) {
+  @media (min-width: ${props => props.dimensions.ultraHd}) {
     margin-left: 35%;
     width: 400px;
   }
@@ -231,7 +219,7 @@ export const TitleWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   text-align: center;
-  background-color: ${ secondaryColor };
+  background-color: ${secondaryColor};
   border-top-left-radius: 0.5em;
   border-top-right-radius: 0.5em;
 `
@@ -243,12 +231,12 @@ interface values {
 }
 
 export const TitleWrapperItem = styled.div<values>`
-  background-color: ${ props => props.loginView ? props.primaryColor : props.secondaryColor };
-  border-top-left-radius: ${ props => props.loginView ? '0.5em' : '0.5em' };
-  border-top-right-radius: ${ props => props.loginView ? '0.5em' : '0.5em' };
+  background-color: ${props => props.loginView ? props.primaryColor : props.secondaryColor};
+  border-top-left-radius: ${props => props.loginView ? '0.5em' : '0.5em'};
+  border-top-right-radius: ${props => props.loginView ? '0.5em' : '0.5em'};
 
   &:hover {
-    background-color: ${ primaryColor };
+    background-color: ${primaryColor};
     border-top-left-radius: 0.5em;
     border-top-right-radius: 0.5em;
   }
--- src/view/signin/components/SignIn.tsx
+++ src/view/signin/components/SignIn.tsx
@@ -4,36 +4,36 @@ import { SignInProps } from '../../../shared/interfaces/SingInInterface'
 import { primaryColor } from '../../../shared/styles/GlobalStyle'
 import { Button, Form, Header1, Input, RowWrapper } from '../../../shared/styles/SiginStyles'
 
-export const SignIn: FC<SignInProps> = ({data}: SignInProps) => {
+export const SignIn: FC<SignInProps> = ({ data }: SignInProps) => {
 	const [loginUsername, setLoginUsername] = useState<string>('')
 	const [loginPassword, setLoginPassword] = useState<string>('')
 	const [loginButton] = useState<string>('Logga In')
-	
+
 	const sendDataToParent = (event: React.MouseEvent<HTMLElement>) => {
 		data.logInUser(loginUsername, loginPassword, event)
 	}
 	return (
 		<RowWrapper>
-			<Header1>{ data.loginHeaderText }</Header1>
+			<Header1>{data.loginHeaderText}</Header1>
 			<Form>
 				<Input
 					type="text"
-					placeholder={ data.usernameText }
-					onChange={ (event) => {
+					placeholder={data.usernameText}
+					onChange={(event) => {
 						setLoginUsername(event.target.value)
-					} }/>
+					}} />
 				<Input
 					type="password"
-					placeholder={ data.passwordText }
-					onChange={ (event) => {
+					placeholder={data.passwordText}
+					onChange={(event) => {
 						setLoginPassword(event.target.value)
-					} }/>
-				<Paragraph>Glömt lösenordet? <Span onClick={ () => {
+					}} />
+				<Paragraph>Glömt lösenordet? <Span onClick={() => {
 					data.changeRecoverPasswordView()
-				} }>Klicka här!</Span></Paragraph>
-				<Button onClick={ (event) => {
+				}}>Klicka här!</Span></Paragraph>
+				<Button onClick={(event) => {
 					sendDataToParent(event)
-				} }>{ loginButton }</Button>
+				}}>{loginButton}</Button>
 			</Form>
 		</RowWrapper>
 	)
@@ -46,7 +46,7 @@ const Paragraph = styled.p`
 
 const Span = styled.span`
   font-family: AlegreyaSansRegular, sans-serif;
-  color: ${ primaryColor };
+  color: ${primaryColor};
   margin-left: 10px;
   font-style: italic;
   cursor: pointer;
