e3ce16566a44e89fdfb8e6c54743a953ab1a9b7f
arasto <Arasto.Sahbaei@gmail.com>
2021-07-09 14:07:19 +0200

    can now sign in

--- src/view/signin/components/SignIn.tsx
+++ src/view/signin/components/SignIn.tsx
@@ -1,43 +1,56 @@
-import React, { FC, useState } from 'react'
-import styled from 'styled-components'
+import React, { FC, useContext, useState } from 'react'
 import { SignInProps } from '../../../shared/interfaces/SingInInterface'
+import { LoginCredentials } from '../../../shared/interfaces/UserInterface'
+import { UserContext } from '../../../shared/providers/UserProvider'
 import { primaryColor } from '../../../shared/styles/GlobalStyle'
 import { Button, Form, Header1, Input, RowWrapper } from '../../../shared/styles/SiginStyles'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import RoutingPath from '../../../routes/RoutingPath'
+import CodicAPIService from '../../../shared/api/services/CodicAPIService'
+import LocalStorage from '../../../shared/cache/LocalStorage'
 
-export const SignIn: FC<SignInProps> = ({ data }: SignInProps) => {
-	const [loginUsername, setLoginUsername] = useState<string>('')
-	const [loginPassword, setLoginPassword] = useState<string>('')
+export const SignIn: FC<SignInProps> = () => {
+	const history = useHistory()
+	const [, setAuthenticatedUser] = useContext(UserContext)
+	const [loginCredentials, setLoginCredentials] = useState<LoginCredentials>({ username: '', password: '' })
 
-	const sendDataToParent = (event: React.MouseEvent<HTMLElement>) => {
-		data.logInUser(loginUsername, loginPassword, event)
+	/* 	const sendDataToParent = (event: React.MouseEvent<HTMLElement>) => {
+			data.logInUser(loginUsername, loginPassword, event)
+		} */
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>, target: keyof LoginCredentials) => {
+		setLoginCredentials({ ...loginCredentials, [target]: event.target.value })
 	}
+
+	const signIn = async (event: React.MouseEvent<HTMLElement>) => {
+		event.preventDefault()
+		try {
+			const { data } = await CodicAPIService.login(loginCredentials)
+			localStorage.setItem(LocalStorage.authenticationToken, data.token)
+			console.log(data)
+			setAuthenticatedUser(data)
+			history.push(RoutingPath.initialView)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	return (
 		<RowWrapper>
-			<Header1>{data.loginHeaderText}</Header1>
+			<Header1>{'Logga in'}</Header1>
 			<Form>
 				<Input
-					type="text"
-					placeholder={data.usernameText}
-					onChange={(event) => {
-						setLoginUsername(event.target.value)
-					}} />
+					type='text'
+					placeholder={'Användarnamn'}
+					onChange={(event) => { handleChange(event, 'username') }} />
 				<Input
-					type="password"
-					placeholder={data.passwordText}
-					onChange={(event) => {
-						setLoginPassword(event.target.value)
-					}} />
-				<Paragraph onClick={() => { data.changeRecoverPasswordView() }}>Glömt lösenordet?</Paragraph>
-				<Button onClick={(event) => { sendDataToParent(event) }}>Logga In</Button>
+					type='password'
+					placeholder={'Lösenord'}
+					onChange={(event) => { handleChange(event, 'password') }} />
+				{/* 	<Paragraph onClick={() => { data.changeRecoverPasswordView() }}>Glömt lösenordet?</Paragraph> */}
+				<Button onClick={(event) => { signIn(event) }}>Logga In</Button>
 			</Form>
 		</RowWrapper>
 	)
 }
-
-const Paragraph = styled.p`
-  font-family: AlegreyaSansRegular, sans-serif;
-  color: ${primaryColor};
-  margin-left: 10px;
-  font-style: italic;
-  cursor: pointer;
-`
