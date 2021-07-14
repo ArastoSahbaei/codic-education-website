5dcb2b178cdbf063993d4c973c6da3b34024c2ae
Lars Strömberg <stromberg.lars@gmail.com>
2021-07-14 05:05:44 +0200

    Change to Formik tag and style

--- src/view/contact/components/ContactForm.tsx
+++ src/view/contact/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { useFormik } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import React from 'react'
 import styled from 'styled-components'
 import * as Yup from 'yup'
@@ -13,76 +13,62 @@ const minLength = (length: number) => {
 
 export const ContactForm = () => {
 	
-	const formik = useFormik({
-		initialValues: {
-			firstName: '',
-			lastName: '',
-			email: '',
-			message: '',
-		},
-		validationSchema: Yup.object({
-			firstName: Yup.string()
-				.min(minLengthName, minLength(minLengthName))
-				.required(required),
-			lastName: Yup.string()
-				.min(minLengthName, minLength(minLengthName))
-				.required(required),
-			email: Yup.string()
-				.email('Ogiltig email adress')
-				.required(required),
-			message: Yup.string()
-				.min(minLengthMessage, minLength(minLengthMessage))
-				.required(required),
-		}),
-		onSubmit: values => {
-			alert(JSON.stringify(values, null, 2))
-		}
-	})
-	
 	return (
-		<form onSubmit={ formik.handleSubmit }>
-			<label htmlFor='firstName'><p>Förnamn</p></label>
-			<Input
-				id='firstName'
-				type='text'
-				autoComplete='on'
-				{...formik.getFieldProps('firstName')}
-			/>
-			{ formik.touched.firstName && formik.errors.firstName ? <div>{ formik.errors.firstName }</div> : null }
-			
-			<label><p>Efternamn</p></label>
-			<Input
-				id='lastName'
-				type='text'
-				autoComplete='on'
-				{...formik.getFieldProps('lastName')}
-			/>
-			{ formik.touched.lastName && formik.errors.lastName ? <div>{ formik.errors.lastName }</div> : null }
-			
-			<label><p>E-Post</p></label>
-			<Input
-				id='email'
-				type='email'
-				autoComplete='on'
-				{...formik.getFieldProps('email')}
-			/>
-			{ formik.touched.email && formik.errors.email ? <div>{ formik.errors.email }</div> : null }
-			
-			<label><p>Meddelande</p></label>
-			<Input
-				id='message'
-				type='text'
-				autoComplete='on'
-				{...formik.getFieldProps('message')}
-			/>
-			{ formik.touched.message && formik.errors.message ? <div>{ formik.errors.message }</div> : null }
-			<br/>
-			
-			<button type='submit'>Skicka</button>
-		</form>
+		<Formik
+			initialValues={ {firstName: '', lastName: '', email: '', message: ''} }
+			validationSchema={ Yup.object({
+				firstName: Yup.string()
+					.min(minLengthName, minLength(minLengthName))
+					.required(required),
+				lastName: Yup.string()
+					.min(minLengthName, minLength(minLengthName))
+					.required(required),
+				email: Yup.string()
+					.email('Ogiltig email adress')
+					.required(required),
+				message: Yup.string()
+					.min(minLengthMessage, minLength(minLengthMessage))
+					.required(required),
+			}) }
+			onSubmit={ values => {
+				alert(JSON.stringify(values, null, 2))
+			} }>
+			<Form>
+				<label htmlFor='firstName'><p>Förnamn</p></label>
+				<Field name='firstName' type='text'/>
+				<ErrorMessage name='firstName'/>
+				
+				<label><p>Efternamn</p></label>
+				<Field name='lastName' type='text'/>
+				<ErrorMessage name='lastName'/>
+				
+				<label><p>E-Post</p></label>
+				<Field name='email' type='email'/>
+				<ErrorMessage name='email'/>
+				
+				<label><p>Meddelande</p></label>
+				<Field name='message' type='text'/>
+				<ErrorMessage name='message'/>
+				
+				<br/>
+				
+				<button type='submit'>Skicka</button>
+			</Form>
+		
+		</Formik>
 	)
 }
 
 const Input = styled.input`
 
+`
+const StyledErrorMessage = styled.div`
+  font-size: 0.8em;
+  color: red;
+  margin-top: 0.25rem;
+
+  &:before {
+    content: "❌ ";
+    font-size: 0.7em;
+  }
 `
