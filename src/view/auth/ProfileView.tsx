344ffb02a2e6420dcd2f5a8a9979c36e6e2d3a83
arasto <Arasto.Sahbaei@gmail.com>
2021-07-20 23:19:48 +0200

    saved

--- src/view/auth/ProfileView.tsx
+++ src/view/auth/ProfileView.tsx
@@ -1,7 +1,9 @@
 export const ProfileView = () => {
 	return (
 		<div>
-			<h1>Profile View</h1>
+			<h1>1. data and update info</h1>
+			<h1>2. change password</h1>
+			<h1>3. inactivate account</h1>
 		</div>
 	)
 }
