48ef8a81eadb56f5f438ad8b1762d3d6ff14d74f
arasto <Arasto.Sahbaei@gmail.com>
2021-07-27 13:30:34 +0200

    modified

--- src/view/contact/components/ContactForm.tsx
+++ src/view/contact/components/ContactForm.tsx
@@ -6,6 +6,8 @@ import { Input } from '../../../components/html/Input'
 import { toast } from 'react-toastify'
 import Validations from 'shared/validations/Validations'
 import CodicAPIService from 'shared/api/services/CodicAPIService'
+import styled from 'styled-components'
+import { primaryColor, primaryFont, secondaryFont } from 'shared/styles/GlobalStyle'
 
 export const ContactForm = () => {
 	const handleSubmit = async (values: IContactForm) => {
@@ -13,25 +15,67 @@ export const ContactForm = () => {
 			await CodicAPIService.sendContactEmail(values)
 			toast.success('✔️ Tack för ditt meddelande. Vi kommer att återkoppla till dig inom snar tid.')
 		} catch (error) {
+			toast.error('Ett fel uppstod')
 			console.log(error)
 		}
 	}
 
 	return (
-		<Formik
-			initialValues={{ name: '', email: '', subject: '', message: '' }}
-			validationSchema={Validations.contactFormValidation}
-			onSubmit={(values, actions) => { handleSubmit(values) && actions.resetForm() }}>
-			<Form autoComplete="off">
-				<Input name='name' label='Namn' type='text' required />
-				<Input name='email' label='Email' type='email' required />
-				<Input name='subject' label='Ärende' type='text' required />
-				<br />
-				<TextArea name='message' type='textarea' placeholder='Meddelande' />
-				<Button text='Skicka' />
-			</Form>
-		</Formik>
+		<Wrapper>
+			<MainParagraph>Kom i kontakt!</MainParagraph>
+			<SubParagraph>
+				Vi svarar gärna på frågor angående IT-konsulter, utbildare eller andra eventuella samarbeten i Göteborg. <br />
+				Fyll gärna i formuläret här nedan eller hör av dig direkt till någon av våra medarbetare så återkopplar vi så fort som möjligt.
+			</SubParagraph>
+			<Formik
+				initialValues={{ name: '', email: '', subject: '', message: '' }}
+				validationSchema={Validations.contactFormValidation}
+				onSubmit={(values, actions) => { handleSubmit(values) && actions.resetForm() }}>
+				<Form autoComplete="off">
+					<Input name='name' label='Namn' type='text' required />
+					<Input name='email' label='Email' type='email' required />
+					<Input name='subject' label='Ärende' type='text' required />
+					<br />
+					<TextArea name='message' type='textarea' placeholder='Meddelande' />
+					<Button text='Skicka' />
+				</Form>
+			</Formik>
+		</Wrapper>
 	)
 }
+
+
+const Wrapper = styled.div`
+	width: 60%;
+	margin: 0 auto;
+	padding: 5%;
+`
+
+const MainParagraph = styled.p`
+	font-family: ${primaryFont};
+	color: ${primaryColor};
+	font-weight: 700;
+	font-size: 32px;
+	line-height: 1em;
+	font-weight: 400;
+	font-style: normal;
+	font-size: 115px;
+	letter-spacing: 0em;
+	line-height: .9em;
+	text-transform: none;
+`
+
+const SubParagraph = styled.p`
+	font-family: ${secondaryFont};
+	font-size: 16px;
+	line-height: 1.6em;
+	font-weight: 400;
+	font-style: normal;
+	font-size: 19px;
+	letter-spacing: 0em;
+	line-height: 1.8em;
+	text-transform: none;
+	color: #212121;
+`
 
 
