bf9106db677002b4b4121c275c3ba97acc9020e6
arasto <Arasto.Sahbaei@gmail.com>
2021-09-21 08:19:11 +0200

    replaced to useAuthentication

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { useEffect, useContext } from 'react'
-import { nonAuthenticatedUser } from '../shared/data/nonAuthenticatedUser'
 import { UserContext } from '../shared/providers/UserProvider'
 import { NewsLetterSubscriptionView } from 'view/auth/newslettersubscriptionview/NewsLetterSubscriptionView'
 import { RetrieveLostPasswordView } from 'view/retrieve-lost-password/RetrieveLostPasswordView'
@@ -8,7 +7,7 @@ import { PersonalInformationView } from 'view/auth/personalinformationview/Perso
 import { UserInformationView } from 'view/auth/userinformationview/UserInformationView'
 import { PurchaseHistoryView } from 'view/auth/purchasehistoryview/PurchaseHistoryView'
 import { ProductDetailView } from '../view/productdetail/ProductDetailView'
-import { validateToken } from 'functions/validateToken'
+import { useAuthentication } from 'hooks/useAuthentication'
 import { CheckoutView } from '../view/checkout/CheckoutView'
 import { EmployeeView } from '../view/employee/EmployeeView'
 import { Error404View } from '../view/error404/Error404View'
@@ -20,14 +19,13 @@ import { AdminView } from '../view/admin/AdminView'
 import { OrderView } from '../view/order/OrderView'
 import { ShopView } from '../view/shop/ShopView'
 import { Footer } from '../components/Footer'
-import CodicAPIService from '../shared/api/services/CodicAPIService'
-import LocalStorage from '../shared/cache/LocalStorage'
 import RoutingPath from './RoutingPath'
 import AdminPath from './AdminPath'
 import AuthPath from './AuthPath'
 
 export const Routes = (props: { children: React.ReactChild[] }) => {
 	const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
+	const { validateUser } = useAuthentication()
 
 	const blockRouteIfAuthenticated = (navigateToViewIfAuthenticated: React.FC) => {
 		return authenticatedUser.authenticated ? InitialView : navigateToViewIfAuthenticated
@@ -37,31 +35,8 @@ export const Routes = (props: { children: React.ReactChild[] }) => {
 		return authenticatedUser.authenticated ? navigateToViewifAuthenticated : SignInView
 	}
 
-	const parseJWT = async () => {
-		const token = localStorage.getItem(LocalStorage.authenticationToken)
-		if (!token) { return }
-		const base64Url = token.split('.')[1]
-		const base64 = base64Url.replace('-', '+').replace('_', '/')
-		const JWT = JSON.parse(window.atob(base64))
-
-		if (validateToken(JWT.exp)) {
-			// TODO: There has to be a better way to recieve the username? You cannot just do a getUserWithID like this?
-			// TODO: Sign in with a new token instead of recieving the user with getUserWithID?
-			try {
-				const { data } = await CodicAPIService.getUserWithID(JWT.id)
-				// TODO: recieve the authenticatid variable from server so that only data can be set
-				setAuthenticatedUser({ ...data, authenticated: true })
-			} catch (error) {
-				console.log(error)
-			}
-		} else {
-			setAuthenticatedUser(nonAuthenticatedUser)
-			localStorage.removeItem(LocalStorage.authenticationToken)
-		}
-	}
-
 	useEffect(() => {
-		parseJWT()
+		validateUser()
 	}, [])
 
 	return (
