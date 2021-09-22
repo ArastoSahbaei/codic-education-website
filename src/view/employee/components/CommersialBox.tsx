56eed48bd74f36f926bb12d24fe73ae16faa1361
Ulrika L <u.lifvenborg@outlook.com>
2021-09-22 16:04:30 +0200

    Test backgroundimage

--- src/view/employee/components/CommersialBox.tsx
+++ src/view/employee/components/CommersialBox.tsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom'
 import RoutingPath from 'routes/RoutingPath'
 import styled from 'styled-components'
+import bImage from '../../../shared/images/brownDesk.jpg'
 
 export const CommersialBox = () => {
 
 	return (
-		<>
+		<div style={{ backgroundImage: `url("${bImage}")`, backgroundSize: '100% 100%'}}>
 			<BigText>Hej! <br /> Vill du bli en del av detta glada gäng?</BigText>
 
 			<SmallText> Titta då närmare på våra lediga tjänster, <Link to={RoutingPath.careerView}> här</Link> ! </SmallText>
-		</>
+		</div>
 	)
 }
 
@@ -17,11 +18,11 @@ export const CommersialBox = () => {
 const BigText = styled.p`
     font-size: 2.5rem;
     text-align:center;
-    margin: 40px 20px 15px 20px;
+    padding: 40px 20px 15px 20px;
 `
 
 const SmallText = styled.p`
     font-size: 1.5rem;
     text-align: center;
-    margin: 15px 20px;
+    padding: 15px 20px 75px 20px;
 `
