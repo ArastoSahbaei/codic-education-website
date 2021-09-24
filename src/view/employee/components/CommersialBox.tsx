f4847528b8478f803846ab19fe4cfd881af58306
Ulrika L <u.lifvenborg@outlook.com>
2021-09-24 07:59:41 +0200

    replace link with useHistory

--- src/view/employee/components/CommersialBox.tsx
+++ src/view/employee/components/CommersialBox.tsx
@@ -1,28 +1,42 @@
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { primaryColor } from 'shared/styles/GlobalStyle'
 import RoutingPath from 'routes/RoutingPath'
 import styled from 'styled-components'
 import bImage from '../../../shared/images/brownDesk.jpg'
 
 export const CommersialBox = () => {
+    const history = useHistory()
 
 	return (
-		<div style={{ backgroundImage: `url("${bImage}")`, backgroundSize: '100% 100%'}}>
+		<BoxWrapper >
 			<BigText>Hej! <br /> Vill du bli en del av detta glada gäng?</BigText>
 
-			<SmallText> Titta då närmare på våra lediga tjänster, <Link to={RoutingPath.careerView}> här</Link> ! </SmallText>
-		</div>
+			<SmallText> Titta då närmare på våra lediga tjänster, <CareerLink onClick={() => history.push(RoutingPath.careerView)}> här</CareerLink> ! </SmallText>
+		</BoxWrapper>
 	)
 }
 
+const BoxWrapper = styled.div`
+    background-image: url(${bImage});
+    background-size: cover;
+`
 
 const BigText = styled.p`
+    color: #f7b045;
     font-size: 2.5rem;
     text-align:center;
-    padding: 40px 20px 15px 20px;
+    padding: 15px 20px 10px 20px;
 `
 
 const SmallText = styled.p`
+    color: #f7b045;
     font-size: 1.5rem;
     text-align: center;
-    padding: 15px 20px 75px 20px;
+    padding: 5px 20px 50px 20px;
+`
+
+const CareerLink = styled.span`
+    color: ${primaryColor};
+    text-decoration: underline;
+    cursor: pointer;
 `
