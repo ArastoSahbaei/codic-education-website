6c30e9187dad555518146e8d00f9cb06544be41f
Ulrika L <u.lifvenborg@outlook.com>
2021-09-30 16:19:59 +0200

    Go to initialView when click on logo

--- src/components/navigation/mobilenavigation/MobileNavigation.tsx
+++ src/components/navigation/mobilenavigation/MobileNavigation.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { HamburgerButton } from './hamburgerbutton/HamburgerButton'
 import { fadeInRight } from 'shared/styles/animations/fadeInRight'
 import { Cart } from 'components/cart/Cart'
@@ -6,17 +7,19 @@ import { BackDrop } from '../../BackDrop'
 import { SideBar } from './sidebar/SideBar'
 import logotype from 'shared/images/codiclogotype.svg'
 import styled from 'styled-components'
+import RoutingPath from 'routes/RoutingPath'
 
 export const MobileNavigation: React.FC = (): JSX.Element => {
 	const [openDrawer, setOpenDrawer] = useState<boolean>(false)
 	const [isCartOpen, setIsCartOpen] = useState<boolean>(false)
+	const history = useHistory()
 
 	return (
 		<Div>
 			<HamburgerButton drawerHandler={setOpenDrawer} />
 			<Cart isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen} />
 			{isCartOpen && <BackDrop drawerHandler={setIsCartOpen} />}
-			<Image src={logotype} />
+			<Image src={logotype} onClick={()=> history.push(RoutingPath.initialView)} />
 			<SideBar drawerIsOpen={openDrawer} drawerHandler={setOpenDrawer} setIsCartOpen={setIsCartOpen} />
 			{openDrawer && <BackDrop drawerHandler={setOpenDrawer} />}
 		</Div>
