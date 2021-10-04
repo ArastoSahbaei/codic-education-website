e1b5a10c5a40d67f07ea8f3ff1c1563751a241bb
arasto <Arasto.Sahbaei@gmail.com>
2021-10-04 16:33:11 +0200

    policy updated

---
+++ src/view/information/privacypolicy/PrivacyPolicyView.tsx
@@ -0,0 +1,163 @@
+import { primaryColor } from 'shared/styles/GlobalStyle'
+import styled from 'styled-components'
+
+export const PrivacyPolicyView = () => {
+
+	return (
+		<Wrapper>
+			<Header>Integritetspolicy</Header>
+			<Title>How we manage your personal data </Title>
+			<Text>
+				This Privacy Policy contains information about how we handle your personal data. In order for you to get a better understanding of how we handle information about you, we have outlined the founding principles of our practices. We ask you to read our Privacy Policy thoroughly before consenting and allowing us to handle your data.
+			</Text>
+			<ol>
+				<li>Founding principles</li>
+				<li>Data controller: Codic Consulting AB</li>
+				<li>What information do we keep?</li>
+				<li>Information from other sources</li>
+				<li>How do we use the information?</li>
+				<li>Consent in regards to newsletters, direct marketing and continued contact</li>
+				<li>For how long do we store data?</li>
+				<li>Who can access the data?</li>
+				<li>How we use cookies</li>
+				<li>Where do we store the information and how do we protect it?</li>
+				<li>You have the right to access the information we have about you</li>
+				<li>How to contact Codic Consulting AB</li>
+				<li>Links to other websites </li>
+			</ol>
+
+			<Title>1. Founding principles</Title> <br />
+			<Text>
+				Codic Consulting AB pledges to respect and protect your personal data and integrity according to the General Data Protection Regulation (GDPR), industry regulations and other relevant norms. To get a better understanding of how we handle information about you, we have outlined
+				the founding principles of our personal data operations in this Privacy Policy. <br /><br />
+				Personal data is information that by itself or together with other data could be used to identify, locate or
+				contact an individual. Names, phone numbers, email, photographs and IP addresses are examples of personal data. <br /><br />
+				Management of personal data refers to all kinds of practices involving personal data, for example: collection, analysis, registration or accumulation.
+				The data controller determines what personal data is used as well as the purpose of using the data. It is the data controller who is ultimately responsible for the management of your personal data, making sure the operations are executed in accordance with the GDPR. </Text>
+			<Title>2. Data controllers: Codic Education AB</Title> <br />
+			<Text>The data controller is Codic Consulting  AB,  in charge of  the management of your personal data. </Text>
+			<Title>3. What information do we keep?</Title> <br />
+			<Text>
+				On Codic Consulting AB’s website, you are able to register your details.That is how we receive information about you, such as your name and how to contact you. <br /><br />
+				To improve our services and communication in accordance with your preferences, we also ask you - aside of the aforementioned - to leave information that may include personal or professional interests or your place of living. <br /><br />
+				When you contact us, apply for a job or register on our website, you can be asked to leave information about yourself.Depending on the situation, we could ask you for the following personal data:
+			</Text>
+			<Title>4. Information from other sources</Title> <br />
+			<Text>
+				When you give us your consent and allow us to handle your personal data,
+				you also allow us to register other information about you that you may have provided in another context.Based on public information,
+				we may supplement your registered data with your working title, industry, information about previous employment or other contact information.
+				If you are our client or looking for consultant opportunities, we could add information about how to contact you from previous job applications or client projects.
+			</Text>
+			<Title>5. How do we use the information?</Title> <br />
+			<Text>
+				Codic Education AB handles and uses the information you provide in order to improve <br /><br />
+				our services and adapt our organisation in accordance with your preferences, for example by offering information about new consultant opportunities or by making the website as user-friendly as possible.  <br /><br />
+				Your personal data is used for the following specified purposes:
+
+				<ul>
+					<li>Sales and marketing, such as direct messaging and/or phone contact.</li>
+					<li>Client care and information strategies regarding offers and job opportunities.</li>
+					<li>To access statistics and information about user behaviour with the final objective of improving our website and user experience.</li>
+					<li>To give you a more personal experience and inform you of consultant projects and other content that may interest you.</li>
+				</ul>
+
+				<br />
+				<br />
+				We may ask for your contact details in order to:
+				<ul>
+					<li>Answer questions and job applications</li>
+					<li>Send information that may interest you</li>
+					<li>Add your name and email address to our mailout lists for newsletters and other content you have chosen to receive</li>
+					<li>Send requested content, or in other ways fulfilling the task we have undertaken in exchange for your data</li>
+					<li>Establish and continue contact</li>
+				</ul>
+
+				<br />
+				Codic Consulting AB will not, without your consent, forward data about you other than what is required by law or in connection to an order of a product or service.
+				<br />
+				<br />
+				Codic Consulting AB will not intentionally exploit information from children or minors and do not pursue direct marketing targeted at children or minors.
+
+			</Text>
+			<Title>6. Consent in regards to newsletters, direct marketing and continued contact</Title> <br />
+			<Text>
+				When you give us your consent to handle your personal data in line with the information above, you agree with the following:
+				<ul>
+					<li>We handle your personal data according to this Privacy Policy</li>
+					<li>We are allowed to provide you with direct marketing about our services via email</li>
+					<li>We are allowed to contact you via email or phone</li>
+					<li>Subscribe to the type of emails you have chosen to receive. You can update your subscription preferences and choose not to receive further emails by clicking on the link at the bottom of every email, or by contacting us directly via email or phone.</li>
+				</ul>
+			</Text>
+			<Title>7. For how long do we store data?</Title> <br />
+			<Text>
+				We handle personal data for a maximum of three years, thereafter we delete the data. If you give us your consent to continue handling your personal data, we will store your data for another three years.
+			</Text>
+			<Title>8. Who can access the personal data?</Title> <br />
+			<Text>
+				Received data will be accessible for a limited number of people at the departments of recruitment, marketing and sales at Codic Consulting AB and its sister companies Future Skill AB and Software Skills International AB.
+			</Text>
+			<Title>9. How we use cookies</Title> <br />
+			<Text>
+				A cookie file is part of a text that - with your consent - can be placed in your computer’s hard drive. Your browser will then add the text in a separate file. Codic Consulting AB uses cookies to facilitate the traffic analyses on our websites by accessing information about who has visited a certain website at a certain time. In the settings of your browser, you can choose to accept cookies or not. You are also able to erase cookie files that have been placed in your hard drive.
+				<br />
+				<br />
+				When you visit the sections of Codic Consulting AB’s website that are adapted after your user profile or require a login, your browser must be set to accept cookies. Aside from that, you can visit our websites without disclosing your identity.
+			</Text>
+			<Title>10. Where do we store the information and how do we protect it?</Title> <br />
+			<Text>
+				Codic Consulting AB protects your personal data through technical and organisational safety measures.
+				<br /><br />
+				Your personal information is stored behind secured networks and is only accessible for a limited amount of people who have specific rights to our systems. These individuals are required to keep your information confidential. All information that you provide is encrypted via SSL (Secure Socket Layer technology).
+			</Text>
+			<Title>11. You have the right to access the information we have about you</Title> <br />
+			<Text>
+				Codic Consulting AB endeavours to make sure that all the information we manage is correct. This also applies to the personal data we collect. If data somehow is revealed to be incorrect, we will immediately rectify the problem. You have the right to access your personal data.
+				<br /><br />
+				From the 25th of May 2018, the GDPR will be put into action. From then on you have the right to access the information we have of you in the form of a register excerpt. You can also demand that we rectify erroneous information about you. You also have the right to withdraw your consent if you no longer want us to handle your personal data. If you withdraw your consent, we will erase the information we have of you. Observe that erasing your personal data can affect the delivery of information within the boundaries of a client relationship as well as potentially affect current dialogues.
+				<br /><br />
+				If you want to withdraw your consent or demand a register excerpt, correction or erasure, you can contact us via the email address we have provided in the chapter for contact details. In order to access a register excerpt,  you need to send us an electronic copy of a signed document with your request.
+			</Text>
+			<Title>12. How to contact Codic Education AB</Title> <br />
+			<Text>
+				Codic Consulting AB is the  data controller for the management of your personal data. If you have any queries regarding our operations or if you wish to contact us in order to exercise your rights, you can find our contact details below:
+				<br /><br />
+				Codic Consulting AB <br />
+				Stora Nygatan 13, 411 08 GÖTEBORG <br />
+				+46 (0)739 60 26 01 <br />
+				hi@codic.se
+			</Text>
+			<Title>13. Links to other websites</Title> <br />
+			<Text>
+				Please note that the links that appear on our websites may bring you to other websites that operate under other privacy regulations than what we have outlined in our Privacy Policy.
+			</Text>
+
+		</Wrapper>
+	)
+}
+
+const Wrapper = styled.div`
+	width: 50%;
+	margin: 0 auto;
+`
+
+const Header = styled.h1`
+	font-size: 3rem;
+`
+
+const Title = styled.b`
+	font-size: 1.8rem;
+	color: ${primaryColor};
+`
+
+const Text = styled.p`
+	font-family: none;
+	font-weight: 400;
+	font-style: normal;
+    font-size: 1.2rem;
+    letter-spacing: 0em;
+    line-height: 1.8em;
+    text-transform: none;
+    color: #212121;
+`
