6b3a5fc0e2114227035210e2d11e138b77ee638c
arasto <Arasto.Sahbaei@gmail.com>
2021-10-04 14:07:47 +0200

    updated

--- src/view/admin/AdminView.tsx
+++ src/view/admin/AdminView.tsx
@@ -3,13 +3,13 @@ import AdminPath from 'routes/AdminPath'
 
 export const AdminView = () => {
 	const history = useHistory()
-	
+
 	return (
 		<>
-			<span onClick= {() => history.push(AdminPath.careerAdminView) }>Careers</span><hr />
-			<span onClick= {() => history.push(AdminPath.productAdminView) }>Products</span><hr />
-			<span onClick= {() => history.push(AdminPath.employeeAdminView) }>Employees</span><hr />
-			<span onClick= {() => history.push(AdminPath.userAdminView) }>Users</span><hr />
+			<span onClick={() => history.push(AdminPath.careerAdminView)}>Careers</span> <br />
+			<span onClick={() => history.push(AdminPath.productAdminView)}>Products</span> <br />
+			<span onClick={() => history.push(AdminPath.employeeAdminView)}>Employees</span> <br />
+			<span onClick={() => history.push(AdminPath.userAdminView)}>Users</span> <br />
 		</>
 	)
 }
