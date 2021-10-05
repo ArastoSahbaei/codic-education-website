ba1de9f70f76525209f63e6885803381d28e4f24
arasto <Arasto.Sahbaei@gmail.com>
2021-10-05 11:47:19 +0200

    updated

--- src/components/navigation/mobilenavigation/MobileNavigation.tsx
+++ src/components/navigation/mobilenavigation/MobileNavigation.tsx
@@ -2,43 +2,47 @@ import React, { useState, useEffect } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 import { HamburgerButton } from './hamburgerbutton/HamburgerButton'
 import { fadeInRight } from 'shared/styles/animations/fadeInRight'
-import { Cart } from 'components/cart/Cart'
+import { debounce } from './components/debounce'
 import { BackDrop } from '../../BackDrop'
 import { SideBar } from './sidebar/SideBar'
-import logotype from 'shared/images/codiclogotype.svg'
+import { Cart } from 'components/cart/Cart'
 import logotypeWhite from 'shared/images/codiclogotype_white.svg'
-import styled from 'styled-components'
 import RoutingPath from 'routes/RoutingPath'
-import useScrollPosition from 'hooks/useScrollPosition'
+import logotype from 'shared/images/codiclogotype.svg'
+import styled from 'styled-components'
+
 
 export const MobileNavigation: React.FC = (): JSX.Element => {
 	const [showNavBar, setShowNavBar] = useState<boolean>(true)
 	const [prevScrollPos, setPrevScrollPos] = useState<number>(0)
 	const [openDrawer, setOpenDrawer] = useState<boolean>(false)
 	const [isCartOpen, setIsCartOpen] = useState<boolean>(false)
-	const currentScrollPos = useScrollPosition()
 	const history = useHistory()
 	const location = useLocation()
 	const theme = {
 		bColor: (location.pathname === RoutingPath.initialView) ? 'white' : 'lightgrey',
-		top: showNavBar ? '0px' : '-60px'
+		top: showNavBar ? '0px' : '-260px'
 	}
 
-	const toggleNavbar = () =>{
-		setShowNavBar(prevScrollPos > currentScrollPos || currentScrollPos < 10)
+	const toggleNavbar = debounce(() => {
+		const currentScrollPos = window.pageYOffset
+		const test = currentScrollPos != prevScrollPos
+		test && setShowNavBar((prevScrollPos > currentScrollPos))
 		setPrevScrollPos(currentScrollPos)
-	}
+	}, 500)
 
 	useEffect(() => {
 		toggleNavbar()
-	}, [prevScrollPos, showNavBar])
+		window.addEventListener('scroll', toggleNavbar)
+		return () => window.removeEventListener('scroll', toggleNavbar)
+	}, [toggleNavbar])
 
 	return (
 		<Div theme={theme} >
 			<HamburgerButton drawerHandler={setOpenDrawer} />
 			<Cart isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen} />
 			{isCartOpen && <BackDrop drawerHandler={setIsCartOpen} />}
-			<Image src={location.pathname === RoutingPath.initialView ? logotype : logotypeWhite} onClick={()=> history.push(RoutingPath.initialView)} />
+			<Image src={location.pathname === RoutingPath.initialView ? logotype : logotypeWhite} onClick={() => history.push(RoutingPath.initialView)} />
 			<SideBar drawerIsOpen={openDrawer} drawerHandler={setOpenDrawer} setIsCartOpen={setIsCartOpen} />
 			{openDrawer && <BackDrop drawerHandler={setOpenDrawer} />}
 		</Div>
@@ -50,7 +54,7 @@ const Div = styled.div`
 	justify-content: space-between;
 	padding: 4%;
 	background-color: ${props => props.theme.bColor};
-	position: sticky;
+	position: fixed;
 	top: ${props => props.theme.top};
 	width: 100%;
 	vertical-align: middle;
