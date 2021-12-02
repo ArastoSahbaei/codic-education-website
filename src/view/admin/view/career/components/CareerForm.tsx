a19ce33eb13879e06a9f4fcdcf70e3acc9d7bf0f
Ulrika L <u.lifvenborg@outlook.com>
2021-12-02 09:59:09 +0100

    Inproved css

--- src/view/admin/view/career/components/CareerForm.tsx
+++ src/view/admin/view/career/components/CareerForm.tsx
@@ -69,10 +69,10 @@ export const CareerForm = (props: { setChoice: (arg0: number) => void; chosenRow
 				<Form autoComplete='off'>
 					<CareerWrapper>
 						<h3>Information om tjänsten</h3>
-						<AdminInput name='title' label='Rubrik på annonsen' />
-						<AdminInput name='jobType' label='Typ av tjänst' />
-						<AdminInput name='location' label='Plats' />
-						<AdminInput name='description' label='Beskrivning' type='textarea' />
+						<AdminInput name='title' label='Rubrik på annonsen' type='text' />
+						<AdminInput name='jobType' label='Typ av tjänst' type='text' />
+						<AdminInput name='location' label='Plats' type='text' />
+						<AdminInput as='textarea' rows='4' name='description' label='Beskrivning' />
 						<AdminInput name='lastDate' label='Sista ansökningsdatum' type='date' />
 					</CareerWrapper>
 					<br />
@@ -97,15 +97,27 @@ const Wrapper = styled.div`
 `
 
 const CareerWrapper = styled.div`
+display: block;
+width: 95%;
+margin-left: 3%;
+margin-bottom: 5px;
+padding: 10px;
+border: 2px solid black;
+border-radius: 5px;
+h3 {
 	display: inline-block;
-	width: 95%;
-	margin-left: 3%;
-	margin-bottom: 5px;
-	padding: 10px;
-	border: 2px solid black;
-	border-radius: 5px;
-	h3 {
-		display: inline-block;
-		padding: 5px 10px;
-	}
+	padding: 5px 10px;
+}
+input[type=text] {
+	margin: 5px 0px 10px 0px;
+	width: 100%;
+}
+input[type=date]{
+	margin: 5px 0px 10px 20px;
+	width: 20%;
+}
+textarea{
+	margin: 5px 0px 10px 0px;
+	width: 100%;
+}
 `
